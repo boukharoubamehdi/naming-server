40ae9cb87ff2a922ba595f94fd4015d3f6e2447d
boukharoubamehdi <boukharouba.mehdi@gmail.com>
2022-05-15 14:00:12 -0400

    Initial commit

---
+++ src/test/java/com/mb/rest/webservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mb.rest.webservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
